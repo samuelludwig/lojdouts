3bf0a92107bc050aa10e1d5d6858159fbf9f8ef7
Samuel Ludwig <SLudwig.dev@gmail.com>
2023-03-14 15:20:02 -0400

    Reach baseline functionality

--- bb/lojdouts/lojdouts.clj
+++ bb/lojdouts/lojdouts.clj
@@ -1,27 +1,33 @@
+(require '[babashka.deps :as deps])
+(deps/add-deps '{:deps {org.clojure/clojure  {:mvn/version "1.11.1"},
+                        aero/aero            {:mvn/version "1.1.6"},
+                        borkdude/rewrite-edn {:mvn/version "0.4.6"},
+                        org.babashka/cli     {:mvn/version "0.6.49"}}})
+
 (ns lojdouts.lojdouts
-  (:require [clojure.string :as str]
-            [babashka.fs :as fs]
-            [clojure.edn :as edn]
+  (:require [babashka.fs :as fs]
             [borkdude.rewrite-edn :as r]
             [aero.core :as aero]
-            [babashka.cli :as cli])
+            [babashka.cli :as cli]
+            [clojure.pprint :refer [pprint]]
+            [babashka.process :refer [shell]])
   (:gen-class))
 
-(def windows? (fs/windows?))
-
-(def bb? (System/getProperty "babashka.version"))
-
-(def deps-template (str/triml "
-{:deps {}
- :aliases {}}
-"))
-
-(def bb-template (str/triml "
-{:deps {}
- :tasks
- {
- }}
-"))
+; (def windows? (fs/windows?))
+;
+; (def bb? (System/getProperty "babashka.version"))
+;
+; (def deps-template (str/triml "
+; {:deps {}
+;  :aliases {}}
+; "))
+;
+; (def bb-template (str/triml "
+; {:deps {}
+;  :tasks
+;  {
+;  }}
+; "))
 
 (defn get-env
   ([var-name default] (or (System/getenv var-name) default))
@@ -32,13 +38,13 @@
 (def loadout-file (str config-dir "/lojdouts/loadouts.edn"))
 (def loadouts (aero/read-config loadout-file))
 
-(defn ensure-deps-file
-  [opts]
-  (let [target (:deps-file opts)]
-    (when-not (fs/exists? target)
-      (spit target (if (= "bb.edn" target) bb-template deps-template)))))
+; (defn ensure-deps-file
+;   [opts]
+;   (let [target (:deps-file opts)]
+;     (when-not (fs/exists? target)
+;       (spit target (if (= "bb.edn" target) bb-template deps-template)))))
 
-(defn edn-string [opts] (slurp (:deps-file opts)))
+;(defn edn-string [opts] (slurp (:deps-file opts)))
 
 (defn edn-nodes [edn-string] (r/parse-string edn-string))
 
@@ -77,31 +83,62 @@
     (deep-merge* deps-nodes lo)))
 
 (defn add-loadout!
-  [deps-file loname]
-  (spit deps-file (merge-loadout deps-file loname)))
+  [{:keys [opts]}]
+  ;(pprint opts)
+  (let [deps-file (if (:bb opts) (str (fs/cwd) "/bb.edn") (:deps-file opts))]
+    (spit deps-file
+          (->> opts
+               :loadout
+               keyword
+               (merge-loadout deps-file)))))
 
-(defn list-loadouts! [] (println (keys loadouts)))
+(defn view-loadout!
+  [{:keys [opts]}]
+  (pprint (get loadouts (keyword (:loadout opts)))))
+
+(defn list-loadouts! [_opts] (pprint (keys loadouts)))
+
+(defn edit-loadouts!
+  [_opts]
+  (let [editor-cmd (get-env "EDITOR" (get-env "VISUAL" "vi"))]
+    (shell (format "%s %s" editor-cmd loadout-file))))
 
 (comment
-  (add-loadout! (str (fs/cwd) "/deps.edn") :mysql)
+  (add-loadout! {:bb true, :loadout "mysql"})
   (merge-loadout (str (fs/cwd) "/deps.edn") :mysql)
-  (add-tap println))
+  (add-tap (bound-fn* pprint)))
 
 (def command-table
-  [{:cmds [], :fn (partial add-loadout! "deps.edn"), :args->opts [:loadout]}
-   {:cmds ["bb"], :fn (partial add-loadout! "bb.edn"), :args->opts [:loadout]}])
+  [{:cmds ["add"], :fn add-loadout!, :args->opts [:loadout]}
+   {:cmds ["view"], :fn view-loadout!, :args->opts [:loadout]}
+   {:cmds ["list"], :fn list-loadouts!} {:cmds ["edit"], :fn edit-loadouts!}])
 
 (def spec
-  {:lofile    {:desc "Use a specific loadout-file", :coerce :string, :alias :f},
-   :bb        {:desc   "Act on bb.edn instead of deps.edn",
-               :coerce :boolean,
-               :alias  :b},
-   :deps-file {:desc   "Path to deps/bb.edn file to operate on",
-               :coerce :string,
-               :alias  :d},
-   :loedit    {:desc   "Open up your loadout.edn file in $EDITOR",
-               :coerce :boolean,
-               :alias  :e}})
+  {:lofile    {:desc    "Use a specific loadout-file",
+               :coerce  :string,
+               :alias   :f,
+               :default loadout-file},
+   :bb        {:desc    "Act on bb.edn by default instead of deps.edn",
+               :coerce  :boolean,
+               :alias   :b,
+               :default false},
+   :deps-file {:desc    "Path to deps/bb.edn file to operate on",
+               :coerce  :string,
+               :alias   :d,
+               :default "deps.edn"},
+   :loedit    {:desc    "Open up your loadout.edn file in $EDITOR",
+               :coerce  :boolean,
+               :alias   :e,
+               :default false},
+   :loadout   {:desc "The name of the loadout to use", :coerce :string},
+   :list      {:desc    "List accessible loadouts",
+               :coerce  :boolean,
+               :alias   :l,
+               :default false},
+   :help      {:desc    "Print help",
+               :coerce  :boolean,
+               :alias   :h,
+               :default false}})
 
 ;; Recommending adding a `lojd` alias set as `lojb`, which is set to `lojd
 ;; --bb`
@@ -110,7 +147,8 @@
   [& args]
   (cli/dispatch command-table
                 args
-                {:spec spec, :exec-args {:deps-file "deps.edn"}})
+                {:spec      spec,
+                 :exec-args {:deps-file (str (fs/cwd) "/deps.edn")}})
   nil)
 
 (when (= *file* (System/getProperty "babashka.file"))
