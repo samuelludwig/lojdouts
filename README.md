4bd1a8f88fa86aac661a42b7bc568fd0158a2459
Samuel Ludwig <SLudwig.dev@gmail.com>
2023-03-16 10:23:30 -0400

    Remove some cruft and add some README info

--- README.md
+++ README.md
@@ -1,56 +1,42 @@
-# lojdouts/lojdouts
+# Lojdouts
 
-FIXME: my new application.
+Are you tired of needing to individually remember/lookup the three different 
+libraries (and their versions!) you need when you want to sanely interface with
+a MySQL database in Clojure?
+
+What if you could have a collections of maps, each one defining a dedicated 
+section of your deps/bb.edn file?
+
+Where instead of running
+```bash
+neil dep add com.github.seancorfield/honeysql
+neil dep add com.github.seancorfield/next.jdbc
+neil dep add mysql/mysql-connector-java
+```
+
+You could just run
+```bash
+lojd add mysql
+```
+
+Enter Lojdouts, a babashka script for managing your deps-style projects'
+loadouts.
 
 ## Installation
 
-Download from https://github.com/lojdouts/lojdouts
+(Requires babashka)
+
+1. Clone the lojdouts repo `https://github.com/samuelludwig/lojdouts.git`.
+2. Run the build task via `bb build:clean`.
+3. Copy the shebanged, executable script to wherever you like to put your bins
+via `cp ./target/lojd <my-bin-directory>`.
 
 ## Usage
 
 FIXME: explanation
 
-Run the project directly, via `:exec-fn`:
-
-    $ clojure -X:run-x
-    Hello, Clojure!
-
-Run the project, overriding the name to be greeted:
-
-    $ clojure -X:run-x :name '"Someone"'
-    Hello, Someone!
-
-Run the project directly, via `:main-opts` (`-m lojdouts.lojdouts`):
-
-    $ clojure -M:run-m
-    Hello, World!
-
-Run the project, overriding the name to be greeted:
-
-    $ clojure -M:run-m Via-Main
-    Hello, Via-Main!
-
-Run the project's tests (they'll fail until you edit them):
-
-    $ clojure -T:build test
-
-Run the project's CI pipeline and build an uberjar (this will fail until you edit the tests to pass):
-
-    $ clojure -T:build ci
-
-This will produce an updated `pom.xml` file with synchronized dependencies inside the `META-INF`
-directory inside `target/classes` and the uberjar in `target`. You can update the version (and SCM tag)
-information in generated `pom.xml` by updating `build.clj`.
-
-If you don't want the `pom.xml` file in your project, you can remove it. The `ci` task will
-still generate a minimal `pom.xml` as part of the `uber` task, unless you remove `version`
-from `build.clj`.
-
-Run that uberjar:
-
-    $ java -jar target/lojdouts-0.1.0-SNAPSHOT.jar
-
-If you remove `version` from `build.clj`, the uberjar will become `target/lojdouts-standalone.jar`.
+- `cp ./doc/example-configs/loadouts.edn ~/.config/lojdouts/loadouts.edn`
+- `lojd {list, view, add} [loadout-name] [--bb]`
 
 ## Options
 
--- src/lojdouts/lojdouts.clj
+++ src/lojdouts/lojdouts.clj
@@ -1,12 +1,149 @@
 (ns lojdouts.lojdouts
+  (:require [babashka.fs :as fs]
+            [borkdude.rewrite-edn :as r]
+            [aero.core :as aero]
+            [babashka.cli :as cli]
+            [clojure.pprint :refer [pprint]]
+            [babashka.process :refer [shell]])
   (:gen-class))
 
-(defn greet
-  "Callable entry point to the application."
-  [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
+; (def windows? (fs/windows?))
+;
+; (def bb? (System/getProperty "babashka.version"))
+;
+; (def deps-template (str/triml "
+; {:deps {}
+;  :aliases {}}
+; "))
+;
+; (def bb-template (str/triml "
+; {:deps {}
+;  :tasks
+;  {
+;  }}
+; "))
 
+(defn get-env
+  ([var-name default] (or (System/getenv var-name) default))
+  ([var-name] (get-env var-name nil)))
+
+(def config-dir (or (get-env "XDG_CONFIG_HOME") (fs/expand-home "~/.config")))
+
+(def loadout-file (str config-dir "/lojdouts/loadouts.edn"))
+(def loadouts (aero/read-config loadout-file))
+
+; (defn ensure-deps-file
+;   [opts]
+;   (let [target (:deps-file opts)]
+;     (when-not (fs/exists? target)
+;       (spit target (if (= "bb.edn" target) bb-template deps-template)))))
+
+;(defn edn-string [opts] (slurp (:deps-file opts)))
+
+(defn edn-nodes [edn-string] (r/parse-string edn-string))
+
+(defn nodes-of
+  [edn-file]
+  (-> edn-file
+      slurp
+      edn-nodes))
+
+(defn merge*
+  "Merge values from map `m` into node `node`."
+  [node m]
+  (loop [main-node node
+         m-kvs     (vec m)]
+    (if (empty? m-kvs)
+      main-node
+      (let [[k v] (first m-kvs)]
+        (recur (r/assoc main-node k v) (rest m-kvs))))))
+
+(defn deep-merge*
+  "Recursively merge values from map `b` into node `a`."
+  [a b]
+  (if (map? (r/sexpr a))
+    (merge* a (for [[k v] b] [k (deep-merge* (r/get a k) v)]))
+    b))
+
+(defn merge-loadout
+  "Merge contents of the named loadout into the target deps-file (usually 
+  deps.edn or bb.edn).
+  
+  Returns the new nodes."
+  [deps-file loname]
+  (let [lo         (get loadouts loname)
+        deps-nodes (nodes-of deps-file)]
+    ;(print {:lo lo :nodes deps-nodes})
+    (deep-merge* deps-nodes lo)))
+
+(defn add-loadout!
+  [{:keys [opts]}]
+  ;(pprint opts)
+  (let [deps-file (if (:bb opts) (str (fs/cwd) "/bb.edn") (:deps-file opts))]
+    (spit deps-file
+          (->> opts
+               :loadout
+               keyword
+               (merge-loadout deps-file)))))
+
+(defn view-loadout!
+  [{:keys [opts]}]
+  (pprint (get loadouts (keyword (:loadout opts)))))
+
+(defn list-loadouts! [_opts] (pprint (keys loadouts)))
+
+(defn edit-loadouts!
+  [_opts]
+  (let [editor-cmd (get-env "EDITOR" (get-env "VISUAL" "vi"))]
+    (shell (format "%s %s" editor-cmd loadout-file))))
+
+(comment
+  (add-loadout! {:bb true, :loadout "mysql"})
+  (merge-loadout (str (fs/cwd) "/deps.edn") :mysql)
+  (add-tap (bound-fn* pprint)))
+
+(def command-table
+  [{:cmds ["add"], :fn add-loadout!, :args->opts [:loadout]}
+   {:cmds ["view"], :fn view-loadout!, :args->opts [:loadout]}
+   {:cmds ["list"], :fn list-loadouts!} {:cmds ["edit"], :fn edit-loadouts!}])
+
+(def spec
+  {:lofile    {:desc    "Use a specific loadout-file",
+               :coerce  :string,
+               :alias   :f,
+               :default loadout-file},
+   :bb        {:desc    "Act on bb.edn by default instead of deps.edn",
+               :coerce  :boolean,
+               :alias   :b,
+               :default false},
+   :deps-file {:desc    "Path to deps/bb.edn file to operate on",
+               :coerce  :string,
+               :alias   :d,
+               :default "deps.edn"},
+   :loedit    {:desc    "Open up your loadout.edn file in $EDITOR",
+               :coerce  :boolean,
+               :alias   :e,
+               :default false},
+   :loadout   {:desc "The name of the loadout to use", :coerce :string},
+   :list      {:desc    "List accessible loadouts",
+               :coerce  :boolean,
+               :alias   :l,
+               :default false},
+   :help      {:desc    "Print help",
+               :coerce  :boolean,
+               :alias   :h,
+               :default false}})
+
+;; Recommending adding a `lojd` alias set as `lojb`, which is set to `lojd
+;; --bb`
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (greet {:name (first args)}))
+  (cli/dispatch command-table
+                args
+                {:spec      spec,
+                 :exec-args {:deps-file (str (fs/cwd) "/deps.edn")}})
+  nil)
+
+(when (= *file* (System/getProperty "babashka.file"))
+  (apply -main *command-line-args*))
